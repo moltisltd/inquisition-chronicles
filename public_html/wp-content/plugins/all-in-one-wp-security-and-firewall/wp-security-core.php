cd805ab29bb70f4fb05e8f0da22b0250ccdd4035
Yoda <david.odie@digitalsix.co.uk>
2017-01-18 12:08:23 +0000

    wordpress updates

--- public_html/wp-content/plugins/all-in-one-wp-security-and-firewall/wp-security-core.php
+++ public_html/wp-content/plugins/all-in-one-wp-security-and-firewall/wp-security-core.php
@@ -1,9 +1,13 @@
-<?php 
+<?php
+
+if ( !defined('ABSPATH') ) {
+    exit; // Exit if accessed directly
+}
 
 if (!class_exists('AIO_WP_Security')){
 
 class AIO_WP_Security{
-    var $version = '4.1.9';
+    var $version = '4.2.5';
     var $db_version = '1.8';
     var $plugin_url;
     var $plugin_path;
@@ -124,12 +128,16 @@ class AIO_WP_Security{
     function loader_operations()
     {
         add_action('plugins_loaded',array(&$this, 'plugins_loaded_handler'));//plugins loaded hook
-        $this->debug_logger = new AIOWPSecurity_Logger();
+        
+        $debug_config = $this->configs->get_value('aiowps_enable_debug');
+        $debug_enabled = empty($debug_config) ? false : true;
+        $this->debug_logger = new AIOWPSecurity_Logger($debug_enabled);
+        
         if(is_admin()){
             $this->admin_init = new AIOWPSecurity_Admin_Init();
         }
     }
-    
+
     static function activate_handler()
     {
         //Only runs when the plugin activates
